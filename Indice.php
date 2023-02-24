4332e31c664d3f8de967f59fea2f773c9792ef6a
Dom349 <125379209+Dom349@users.noreply.github.com>
2023-02-24 10:37:29 -0700

    Update Indice.php

--- Indice.php
+++ Indice.php
@@ -1,9 +1,84 @@
+<?php
+include 'funciones.php';
+
+$error = false;
+$config = include 'config.php';
+
+try {
+  $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
+  $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+
+  $consultaSQL = "SELECT * FROM empleados";
+
+  $sentencia = $conexion->prepare($consultaSQL);
+  $sentencia->execute();
+
+  $empleados = $sentencia->fetchAll();
+
+} catch(PDOException $error) {
+  $error= $error->getMessage();
+}
+?>
+
+<?php include "templates/header.php"; ?>
+
+<?php
+if ($error) {
+  ?>
+  <div class="container mt-2">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="alert alert-danger" role="alert">
+          <?= $error ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+}
+?>
+
 <?php include "templates/header.php"; ?>
   <div class="container">
   <div class="row">
     <div class="col-md-12">
       <a href="crear.php"  class="btn btn-primary mt-4">Crear empleado</a>
       <hr>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="mt-3">Lista de empleados</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>telefono</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if ($empleados && $sentencia->rowCount() > 0) {
+            foreach ($empleados as $fila) {
+              ?>
+              <tr>
+                <td><?php echo escapar($fila["id"]); ?></td>
+                <td><?php echo escapar($fila["nombre"]); ?></td>
+                <td><?php echo escapar($fila["apellido"]); ?></td>
+                <td><?php echo escapar($fila["email"]); ?></td>
+                <td><?php echo escapar($fila["telefono"]); ?></td>
+              </tr>
+              <?php
+            }
+          }
+          ?>
+        <tbody>
+      </table>
     </div>
   </div>
 </div>
